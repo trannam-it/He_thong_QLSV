1642166aa8700cd64ba35b9a74db555a537d9fed
stu735105062-jpg <stu735105062@hnue.edu.vn>
2026-01-18 20:02:51 +0700

    Delete nguyenbaluong-master directory

--- nguyenbaluong-master/includes/footer.php
+++
@@ -1,8 +0,0 @@
-    <footer class="bg-dark text-light text-center mt-auto">
-        <div class="position-relative">
-            <a href="../public/logout.php" class="btn btn-danger float-end">
-                <i><i class="bi bi-box-arrow-right"></i></i>
-            </a>
-            <h5>Copyright@PhpWebApp.com</h5>
-        </div>
-    </footer>
